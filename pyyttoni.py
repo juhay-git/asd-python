55d5f18a5cd11e1654b3735d60e6552b7b47a372
VaPezizi <samuli.pihlaja.SP@gmail.com>
2024-09-17 11:10:29 +0300

    spams empty files

--- pyyttoni.py
+++ pyyttoni.py
@@ -1,7 +1,14 @@
 # Lisää oma kommentti ja nimikirjaimet alle
 
 import time
+import system
+import random
 
+# using random.choices() generating random strings
 while True:
+    
+
+    res = ''.join(random.choices(string.ascii_letters,k=7))
+    f = open(res, "x") 
     print("Hello World!")
     time.sleep(1)
